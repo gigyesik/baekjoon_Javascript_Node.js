bb5f234f78e69b04f640b52cf8e0743451a174d3
WoongBeom Heo <98306498+gigyesik@users.noreply.github.com>
2022-02-15 16:24:20 +0900

    dog

---
+++ 01_input_output_and_arithmetic_operation/10172.js
@@ -0,0 +1,8 @@
+// Dog
+// https://www.acmicpc.net/problem/10172
+
+console.log('|\\_/|');
+console.log('|q p|   /}');
+console.log('( 0 )"""\\');
+console.log('|"^"`    |');
+console.log('||_/=\\\\__|');
